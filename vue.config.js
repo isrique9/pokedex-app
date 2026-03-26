4aa1e19a0056aa5453f7bb0ad742ff24478cfe8e
isrique9 <henriqueisrael790@gmail.com>
2026-03-26 11:48:14 -0300

    Teste novo base no config.

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js (Vue 3)
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/pokedex-app/', // <--- MUITO IMPORTANTE: use o nome do seu repo aqui
-})
